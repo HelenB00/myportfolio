22c6f497e33951363cde1877511bb0ccd021dde8
HelenB00 <Helen1031B@outlook.com>
2026-03-10 12:00:03 +0300

    first commit

---
+++ src/data/education.ts
@@ -0,0 +1,16 @@
+import { type ExperienceShowcaseListItemProps } from "@/components/experience/experience-showcase-list-item";
+
+export const EDUCATION: ExperienceShowcaseListItemProps[] = [
+  {
+    title: "Bachelor’s Degree in Computer Science and Technology, ",
+    organisation: {
+      name: "Shanghai University of Engineering Science",
+      href: "https://eng.rizvi.edu.in/",
+    },
+    date: "2018-2022",
+    location: "Shanghai, China",
+    description:
+      // "Relevant courses included Data Structures and Algorithms, Web Design, Cloud Computing, Artificial Neural Network and Database Management.",
+      "Programming Fundamentals, Object-Oriented Programming, Data Structures and Algorithms, Database Systems, Computer Networks, Software Engineering, Web Development."
+  }  
+];
---
+++ src/data/experience.ts
@@ -0,0 +1,37 @@
+import { type ExperienceShowcaseListItemProps } from "@/components/experience/experience-showcase-list-item";
+
+export const EXPERIENCE: ExperienceShowcaseListItemProps[] = [
+  {
+    title: "Senior Full-Stack Developer",
+    organisation: {
+      name: "Skima",
+      href: "https://skima.ai/",
+    },
+    date: "Jan 2024 - Present",
+    location: "Remote",
+    description: "Reactjs, NodeJs and Python app development. Developed and implemented over 8 web applications for startups and corporate clients.",
+  },
+  {
+    title: "Full-Stack Developer",
+    organisation: {
+      name: "Hexaware",
+      href: "https://hexaware.com/",
+    },
+    date: "Mar 2023 - Jan 2024",
+    location: "Remote",
+    description:
+      "Created reusable React components, reducing development time by 30%.Improved database performance by 35% through query and index optimization.",
+  },
+  {
+    // title: "Frontend Developer Intern",
+    title: "Junior Full-Stack Developer",
+    organisation: {
+      name: "Master Infotech",
+      href: "https://masterinfotech.com/",
+    },
+    date: "Apr 2020 - Mar 2023",
+    location: "Remote",
+    description:
+      "Strengthened programming skills in JavaScript, Python, and PHP, laying the foundation for professional development of full-featured applications.Assisted with backend development using PHP, Python, and MySQL, as well as with frontend functionality using JavaScript and React.",
+  },
+];
